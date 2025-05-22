a1c7479ea268f76168eccf043654863ac91c5a1f
Thekra Aljagthmi <thekra.aljagthmi@gmail.com>
2025-05-22 13:22:41 +0300

    Update on JeddahReads

--- src/pages/JeddahReads.jsx
+++ src/pages/JeddahReads.jsx
@@ -4,23 +4,23 @@ import "../assets/styles/HistoricJeddah.css";
 
 function JeddahReads() {
   return (
-    <>
+    
      <div className="historic-container">
-
-         <video
+        <video
         src="/videos/video-3.mp4"
         type="video/mp4"
         className="background-video"
         autoPlay
         loop
         ></video>
+         
 
       <div className="content">
         <h1>جدة تقرأ</h1>
         <p>لأن لكل كتاب حكاية... ولكل قارئ رحلة</p>
       </div>
     </div>
-    </>
+   
   )
 }
 
